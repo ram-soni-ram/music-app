899d7579db26a0e4e230d8a2128f8d3ca5f0db87
ram soni ram <ramsoni888990@gmail.com>
2025-06-15 02:37:17 +0530

    test path

--- music-player/src/songData.js
+++ music-player/src/songData.js
@@ -41,154 +41,154 @@ const songData = {
 			songTitle:
 				"Morni _ Badshah _ Sharvi Yadav _ Preity Mukhundhan _ Hiten(MP3_128K).mp3",
 			songUrl: song1,
-			songPoster: "music-app/images/morni.jpeg"
+			songPoster: "/music-app/images/morni.jpeg"
 		},
 		{
 			id: "2",
 			songTitle:
 				"Aayi Nai -Stree 2 _ Shraddha Kapoor _ Rajkummar Rao _ Sachin-Jigar _Pawan Singh_Simran_Divya_Amitabh(MP3_128K).mp3",
 			songUrl: song2,
-			songPoster: "music-app/images/kati.jpeg"
+			songPoster: "/music-app/images/kati.jpeg"
 		},
 		{
 			id: "3",
 			songTitle:
 				"Sajna Hai Mujhe Jhankar Beats _ Shruti Rane _ DJ Harshit Shah _ DJ MHD IND _ Evergreen Hindi Song(MP3_128K).mp3",
 			songUrl: song3,
-			songPoster: "music-app/images/sajna hai.jpeg"
+			songPoster: "/music-app/images/sajna hai.jpeg"
 		},
 		{
 			id: "4",
 			songTitle:
 				"Bimar Dil _ Pagalpanti _ Urvashi_John_Arshad_Ileana_ Pulkit_ Asees K_ Jubin N_ Tanishk B(MP3_128K).mp3",
 			songUrl: song4,
-			songPoster: "music-app/images/bimar dil.jpeg"
+			songPoster: "/music-app/images/bimar dil.jpeg"
 		},
 		{
 			id: "5",
 			songTitle:
 				"Bekhayali Full Song _ Kabir Singh _ Shahid K_Kiara A_Sandeep Reddy Vanga _ Sachet-Parampara _ Irshad(MP3_128K).mp3",
 			songUrl: song5,
-			songPoster: "music-app/images/bekhayali.jpeg"
+			songPoster: "/music-app/images/bekhayali.jpeg"
 		},
 		{
 			id: "6",
 			songTitle:
 				"Evil Dead Rise Original Trailer Song _ Whatever Will Be_ Will Be (Que Sera_ Sera) _ TRAILER OST(MP3_128K).mp3",
 			songUrl: song6,
-			songPoster: "music-app/images/rise.jpeg"
+			songPoster: "/music-app/images/rise.jpeg"
 		},
 		{
 			id: "7",
 			songTitle:
 				"Kehndi Haan Kehndi Naa - Sukriti_ Prakriti _ Arjun Bijlani _ Siddhant K _ Rishabh K _ VYRL Originals(MP3_128K).mp3",
 			songUrl: song7,
-			songPoster: "music-app/images/kaindi haan.jpeg"
+			songPoster: "/music-app/images/kaindi haan.jpeg"
 		},
 		{
 			id: "8",
 			songTitle:
 				"Oo Bolega ya Oo Oo Bolega Ft Samantha (Full Video) Pushpa _ Allu A_ Rashmika_Kanika K_ DSP_ Sukumar(MP3_128K).mp3",
 			songUrl: song8,
-			songPoster: "music-app/images/o bolega.jpeg"
+			songPoster: "/music-app/images/o bolega.jpeg"
 		},
 		{
 			id: "9",
 			songTitle:
 				"Rang Jo Lagyo _ Ramaiya Vastavaiya _ Atif Aslam _ Shreya Ghoshal _ Bollywood Romantic Hits(MP3_128K).mp3",
 			songUrl: song9,
-			songPoster: "music-app/images/rang jo lagyo.jpeg"
+			songPoster: "/music-app/images/rang jo lagyo.jpeg"
 		},
 		{
 			id: "10",
 			songTitle:
 				"_Tum Hi Ho_ Aashiqui 2 Full Song With Lyrics _ Aditya Roy Kapur_ Shraddha Kapoor(MP3_128K).mp3",
 			songUrl: song10,
-			songPoster: "music-app/images/tum hi ho.jpeg"
+			songPoster: "/music-app/images/tum hi ho.jpeg"
 		},
 		{
 			id: "11",
 			songTitle:
 				"Ye mumkin to nahi jo dil ne chaha tha wo mil jaye   Hayat and Murat(MP3_128K).mp3",
 			songUrl: song11,
-			songPoster: "music-app/images/ye mumkin.jpeg"
+			songPoster: "/music-app/images/ye mumkin.jpeg"
 		},
 		{
 			id: "12",
 			songTitle:
 				"Aastha Gill - Buzz feat Badshah _ Priyank Sharma _ Official Music Video(MP3_128K).mp3",
 			songUrl: song12,
-			songPoster: "music-app/images/buzz.jpeg"
+			songPoster: "/music-app/images/buzz.jpeg"
 		},
 		{
 			id: "13",
 			songTitle:
 				"Aasman Mein Jaise Badal Ho Rahe Hain __ Yaad Piya ki Aane Lagi __ Neha Kakkar Latest Hindi Song 2019(MP3_128K).mp3",
 			songUrl: song13,
-			songPoster: "music-app/images/yaad piya ki.jpeg"
+			songPoster: "/music-app/images/yaad piya ki.jpeg"
 		},
 		{
 			id: "14",
 			songTitle:
 				"Dil Ko Maine Di Kasam Video _ Amaal M Ft.Arijit S_Kumaar _ Asim R_Himanshi K_ Bhushan K _Arvindr K(MP3_128K).mp3",
 			songUrl: song14,
-			songPoster: "music-app/images/dil ki maine di.jpeg"
+			songPoster: "/music-app/images/dil ki maine di.jpeg"
 		},
 		{
 			id: "15",
 			songTitle:
 				"Pal Pal Dil Ke Paas - Title _ Arijit Singh _ Karan Deol_ Sahher _ Parampara_ Sachet_ Rishi Rich(MP3_128K).mp3",
 			songUrl: song15,
-			songPoster: "music-app/images/pal pal dil ke pass.jpeg"
+			songPoster: "/music-app/images/pal pal dil ke pass.jpeg"
 		},
 		{
 			id: "16",
 			songTitle:
 				"Sooraj Dooba Hain_ FULL VIDEO SONG _ Arijit singh Aditi Singh Sharma _ T-SERIES(MP3_128K).mp3",
 			songUrl: song16,
-			songPoster: "music-app/images/sooraj dooba hai.jpg"
+			songPoster: "/music-app/images/sooraj dooba hai.jpg"
 		},
 		{
 			id: "17",
 			songTitle:
 				"KHAIRIYAT (BONUS TRACK) _ CHHICHHORE _ Sushant_ Shraddha _ Pritam_ Amitabh B_Arijit Singh(MP3_128K).mp3",
 			songUrl: song17,
-			songPoster: "music-app/images/khairiyat.jpg"
+			songPoster: "/music-app/images/khairiyat.jpg"
 		},
 		{
 			id: "18",
 			songTitle:
 				"Malang (Title Track)_ Aditya Roy Kapur_ Disha Patani_ Anil K_ Kunal K _ Ved S _ Mohit S(MP3_128K).mp3",
 			songUrl: song18,
-			songPoster: "music-app/images/malang.jpg"
+			songPoster: "/music-app/images/malang.jpg"
 		},
 		{
 			id: "19",
 			songTitle:
 				"Resham Ka Rumal _ Divya Agarwal _ Shruti Rane _ Official Music Video _ Latest Hindi Song 2022(MP3_128K).mp3",
 			songUrl: song19,
-			songPoster: "music-app/images/resham ka.jpeg"
+			songPoster: "/music-app/images/resham ka.jpeg"
 		},
 		{
 			id: "20",
 			songTitle:
 				"Badshah - DJ Waley Babu _ feat Aastha Gill _ Party Anthem _ DJ Wale Babu(MP3_128K).mp3",
 			songUrl: song20,
-			songPoster: "music-app/images/dj wale babu.jpg"
+			songPoster: "/music-app/images/dj wale babu.jpg"
 		},
 		{
 			id: "21",
 			songTitle:
 				"Humraah Full Song _ Malang _ Aditya R K_ Disha P Anil K Kunal K _ Sachet T _ Mohit S _ Fusion P(MP3_128K).mp3",
 			songUrl: song21,
-			songPoster: "music-app/images/humraah.jpeg"
+			songPoster: "/music-app/images/humraah.jpeg"
 		},
 		{
 			id: "22",
 			songTitle:
 				"Disco Balma - Mouni Roy _ Asees Kaur _ Mellow D _ Sachin - Jigar _ IP Singh _ Zee Music Originals(MP3_128K).mp3",
 			songUrl: song22,
-			songPoster: "music-app/images/disco balma.jpeg"
+			songPoster: "/music-app/images/disco balma.jpeg"
 		}
 	],
 	bhojpuriSong: [
@@ -197,21 +197,21 @@ const songData = {
 			songTitle:
 				"Full Song - Gor Kariya - गोर करिया - Pawan Singh - Monalisa - SARKAR RAJ - Bhojpuri Song(MP3_128K).mp3",
 			songUrl: bhSong1,
-			songPoster: "music-app/images/gore kariya.jpeg"
+			songPoster: "/music-app/images/gore kariya.jpeg"
 		},
 		{
 			id: "24",
 			songTitle:
 				"Hum Haeen Piya Ji Ke Patar Tiriywa _ _Bhojpuri SONG _ Patna Se Pakistan(MP3_128K).mp3",
 			songUrl: bhSong2,
-			songPoster: "music-app/images/hum haeen.jpeg"
+			songPoster: "/music-app/images/hum haeen.jpeg"
 		},
 		{
 			id: "25",
 			songTitle:
 				"Haradiya Kaam Na Kari Taza ¦ Khesari Lal Yadav_Kajal Raghwani _ full songs 2019(MP3_128K).mp3",
 			songUrl: bhSong3,
-			songPoster: "music-app/images/hardiya.jpeg"
+			songPoster: "/music-app/images/hardiya.jpeg"
 		}
 	],
 	englishSong: [
@@ -219,14 +219,14 @@ const songData = {
 			id: "26",
 			songTitle: "Rema_ Selena Gomez - Calm Down (Lyrics)(MP3_128K).mp3",
 			songUrl: enSong1,
-			songPoster: "music-app/images/calm down.jpeg"
+			songPoster: "/music-app/images/calm down.jpeg"
 		},
 		{
 			id: "27",
 			songTitle:
 				"KHAIRIYAT - Arijit Singh_ Pritam [English Cover](MP3_128K).mp3",
 			songUrl: enSong2,
-			songPoster: "music-app/images/will you.jpeg"
+			songPoster: "/music-app/images/will you.jpeg"
 		}
 	]
 };
